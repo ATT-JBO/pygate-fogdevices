22d643f409e4da61a6511a7ed0fc25363585e5a6
Jan Bogaerts <jb@allthingstalk.com>
2016-09-12 20:08:00 +0200

    init checkin

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pygate_fogdevices',
+    version='1.0',
+    packages=['pygate_fogdevices'],
+    url='www.allthingstalk.com',
+    license='',
+    author='Jan Bogaerts',
+    author_email='jb@allthingstalk.com',
+    description="plugin for the pygate: provides support for locally managed http/mqtt connected devices"
+)
